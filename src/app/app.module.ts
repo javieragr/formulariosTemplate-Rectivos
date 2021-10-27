57d39f0be7d99e624b9ca886775c642a55780907
javieragr <javieragr_1993@hotmail.com>
2021-10-26 20:05:19 -0700

    termino de la seccion validaciones de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -15,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
 
     
   ],
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, Validator, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, Validator, FormControl, ValidationErrors } from '@angular/forms';
 import { emailPattern, nombreApellidoPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
 import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -25,23 +26,68 @@ export class RegistroComponent implements OnInit {
 
   miFormulario:FormGroup= this.fb.group({
     nombre:['',[Validators.required,Validators.pattern(this.validatorService.nombreApellidoPattern)]],
-    email:['',[Validators.required,Validators.pattern(this.validatorService.emailPattern)]],
-    userName:['',[Validators.required,this.validatorService.noPuedeSerStrider ], ]
+    email:['',[Validators.required,Validators.pattern(this.validatorService.emailPattern)],[this.emailValidator]],
+    userName:['',[Validators.required,this.validatorService.noPuedeSerStrider ], ],
+    password:['',[Validators.required,Validators.minLength(6)] ],
+    verificarPassword:['',[Validators.required], ]
+  },{
+    validators:[this.validatorService.camposIguales('password','verificarPassword')]
+
   })
 
-  constructor(private fb:FormBuilder,private validatorService:ValidatorService) { }
+  // emailErrorMsg:string="";
+  get emailErrorMsg():string{
+    
+    
+    // const error= this.miFormulario.get('email')?.errors;
+    // console.log(error);
+    // if (error?.required) {
+    //   return 'El email es requerido'
+    // }
+    // else if(error?.pattern)
+    // {return 'El email ingresado no cumple con el formato correcto'}
+    // else if (error?.emailTomado) {
+    //   return 'El email ya existe'
+    // }
+    return'fgfff'
+
+  }
+  constructor(private fb:FormBuilder,private validatorService:ValidatorService, 
+    private emailValidator:EmailValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre:'Fernando Herrera',
-      email:'ferni@gmail.com',
-      useName:'usuarioAAA'
+      email:'test1@test.com',
+      useName:'usuarioAAA',
+      password:'123456',
+      verificarPassword:'123456'
+
     })
   }
   campoNoValido(campo:string){
     return this.miFormulario.get(campo)?.invalid
           && this.miFormulario.get(campo)?.touched
   }
+
+
+
+  emailRequired(){
+    return this.miFormulario.get('email')?.errors?.required
+          && this.miFormulario.get('email')?.touched
+  }
+  emailFormato(){
+    return this.miFormulario.get('email')?.errors?.pattern
+          && this.miFormulario.get('email')?.touched
+  }
+  emailTomado(){
+    return this.miFormulario.get('email')?.errors?.emailTomado
+          && this.miFormulario.get('email')?.touched
+  }
+
+
+
+
   submitFormulario(){
     if(this.miFormulario.invalid){
       this.miFormulario.markAllAsTouched();
